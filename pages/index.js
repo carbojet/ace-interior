c8b47f4bb5c803c7850d4769aef5fc7d63a46778
sunil kumar mutaka <carbojet@gmail.com>
2021-05-20 10:33:17 +0530

    add state axios call

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import { Heading, Page } from "@shopify/polaris";
 import React,{ Component } from 'react';
+
 export default class Index extends Component{
+
+  constructor(props) {
+      super(props)  
+      this.state = {
+         theme:null
+      }
+  }
+  componentDidMount(){
+      try{
+          const response = axios({
+              url:'/api/theme',
+              method:'GET',         
+
+          }).then((response) =>{
+              
+            this.setState({
+                theme:response
+            })
+          })
+      }catch(error){
+          console.log(error)
+      }
+  }
   render(){
+    console.log(this.state.theme)
     return(
       <Page>
-      <Heading>Axios Heroku Shopify app with Node and React 🎉</Heading>
-    </Page>
+        <Heading>Axios Heroku Shopify app with Node and React 🎉</Heading>
+      </Page>
     )
   }  
 }
